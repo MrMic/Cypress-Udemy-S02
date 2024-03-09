ade2f8b7eff896e3f0d8df04ecb779fe40aa068b
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-03-09 14:59:14 +0100

    (feat) Tune first test + IDE Cypress autocompletion Config

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="Cypress" />
+
+describe("Tasks page", () => {
+  it("should render the mainimage", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("img").should("be.visible");
+  });
+});
